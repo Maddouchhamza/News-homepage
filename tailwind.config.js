1f98f781cdc38aab31d9643f28ece9dd0dd98421
Maddouchhamza <maddouch.hamza@gmail.com>
2022-11-16 12:49:50 +0100

    Add cntent path on tlwd.cfg.js to fix vercel prblm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html"],
+  content: ["./src/**/*.{html,js}", "./index.html", "./Components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
