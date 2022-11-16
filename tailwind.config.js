a7e4a6db1342dd2eb04f43fdc92b7dcf23583fd7
Maddouchhamza <maddouch.hamza@gmail.com>
2022-11-16 12:22:02 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Inter']
+      },
+      colors: {
+        'Soft-orange': 'hsl(35, 77%, 62%)',
+        'Soft-red': 'hsl(5, 85%, 63%)',
+        'Off-white': 'hsl(36, 100%, 99%)',
+        'Grayish-blue': 'hsl(233, 8%, 79%)',
+        'Dark-grayish-blue': 'hsl(236, 13%, 42%)',
+        'Very-dark-blue': 'hsl(240, 100%, 5%)'
+      }
+    },
+    letterSpacing: {
+      widest: '.25em',
+      wider: '0.03em'
+    }
+  },
+  plugins: [],
+}
