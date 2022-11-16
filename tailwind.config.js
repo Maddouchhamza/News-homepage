57e5c3e807d9bfa4822309479d6140449e192a23
Maddouchhamza <maddouch.hamza@gmail.com>
2022-11-16 13:02:22 +0100

    The content path deleted: it doesn't fix the prblm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html", "./Components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
       fontFamily: {
